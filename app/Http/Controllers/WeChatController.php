c01b6589b99f9b2108dc5011998d51c8d3d48ed7
LiangPi <609671961@qq.com>
2018-01-16 11:43:04 +0000

    wechat

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class WeChatController extends Controller
 {
-    public function oauthCallback(){
-        var_dump(URL::getRequest());
+    public function oauthCallback(Request $request){
+        var_dump($request->url());
     }
 }
