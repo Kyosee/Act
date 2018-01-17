0f133a7492b4da0df48af00c3ec01c72c27b3c11
LiangPi <609671961@qq.com>
2018-01-17 10:11:47 +0000

    wechat

--- database/migrations/2018_01_16_042220_create_users_table.php
+++ database/migrations/2018_01_16_042220_create_users_table.php
@@ -15,9 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('wechat_id');
             $table->string('openid');
             $table->string('nickname');
-            $table->string('avatar');
+            $table->string('avatar')->default('');
+            $table->smallInteger('gender')->default(1);
+            $table->string('language')->default('');
+            $table->string('city')->default('');
+            $table->string('province')->default('');
+            $table->string('country')->default('');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
