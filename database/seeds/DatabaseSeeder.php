a8499e7b0825fb0b3b666eb1ae374ac831d2dde5
LiangPi <609671961@qq.com>
2018-01-20 14:20:19 +0000

    wechat

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ProjectsTableSeeder::class,
             ProjectTemplatesTableSeeder::class,
+            ProjectsTableSeeder::class,
         ]);
     }
 }
