b940bd3caf7d713f2ecd4a247fa5efef0ad8ca95
joy <joy@starfish.ren>
2018-01-23 18:48:27 +0800

    a

---
+++ resources/views/dashboard/templates/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts._default_dashboard')
+@section('title', '应用模板管理')
+@section('content')
+	<div id="page-wrapper">
+		<div class="main-page">
+			<div class="grids">
+				<div class="panel panel-widget">
+					<div class="tables">
+						<h4>Colored Rows Table:</h4>
+						<table class="table"> <thead> <tr> <th>#</th> <th>Column heading</th> <th>Column heading</th> <th>Column heading</th> </tr> </thead> <tbody> <tr class="active"> <th scope="row">1</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr> <th scope="row">2</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr class="success"> <th scope="row">3</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr> <th scope="row">4</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr class="info"> <th scope="row">5</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr> <th scope="row">6</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr class="warning"> <th scope="row">7</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr> <th scope="row">8</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr class="danger"> <th scope="row">9</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> </tbody> </table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
