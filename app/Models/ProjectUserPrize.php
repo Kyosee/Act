c37a2bc92548c4bc20ea89aeb6e417992cd6f915
LiangPi <609671961@qq.com>
2018-01-29 05:06:53 +0000

    wechat

--- app/Models/ProjectUserPrize.php
+++ app/Models/ProjectUserPrize.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ProjectUserPrize extends Model
 {
     public static function createLog($data){
-        self::$uid = $data['uid'] ? $data['uid'] : 0;
-        self::$project_id = $data['project_id'];
-        self::$added = $data['added'] ? $data['added'] : '';
+        self::uid = $data['uid'] ? $data['uid'] : 0;
+        self::project_id = $data['project_id'];
+        self::added = $data['added'] ? $data['added'] : '';
         self::save();
     }
 }
