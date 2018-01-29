73ae0d8bd37e61e7ba49465063d42640734ea8e2
LiangPi <609671961@qq.com>
2018-01-29 05:10:00 +0000

    wechat

--- app/Models/ProjectUserPrize.php
+++ app/Models/ProjectUserPrize.php
@@ -9,7 +9,8 @@ class ProjectUserPrize extends Model
     public function createLog($data){
         $this->uid = $data['uid'] ? $data['uid'] : 0;
         $this->project_id = $data['project_id'];
-        $this->added = $data['added'] ? $data['added'] : '';
+        $this->prize_id = $data['prize_id'] ;
+        $this->exchange = $data['exchange'] ? $data['exchange'] : false;
         $this->save();
     }
 }
