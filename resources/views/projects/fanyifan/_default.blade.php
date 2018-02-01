23e9d00c0bd834ebef585d061faf630a989d187a
LiangPi <609671961@qq.com>
2018-02-01 04:02:54 +0000

    wechat

--- resources/views/projects/fanyifan/_default.blade.php
+++ resources/views/projects/fanyifan/_default.blade.php
@@ -19,29 +19,29 @@
     <script src="/js/bootstrap.min.js"></script>
     <!--移动端版本兼容 -->
     <script type="text/javascript">
-        var phoneWidth = parseInt(window.screen.width);
-        var phoneScale = phoneWidth / 640;
+    var phoneWidth = parseInt(window.screen.width);
+    var phoneScale = phoneWidth / 640;
 
-        var ua = navigator.userAgent;
-        if (/Android (\d+\.\d+)/.test(ua)) {
-            var version = parseFloat(RegExp.$1);
-            // andriod 2.3
-            if (version > 2.3) {
-                document.write('<meta name="viewport" content="width=640, minimum-scale = ' + phoneScale + ', maximum-scale = ' + phoneScale + ', target-densitydpi=device-dpi">');
-                // andriod 2.3以上
-            } else {
-                document.write('<meta name="viewport" content="width=640, target-densitydpi=device-dpi">');
-            }
-            // 其他系统
+    var ua = navigator.userAgent;
+    if (/Android (\d+\.\d+)/.test(ua)) {
+        var version = parseFloat(RegExp.$1);
+        // andriod 2.3
+        if (version > 2.3) {
+            document.write('<meta name="viewport" content="width=640, minimum-scale = ' + phoneScale + ', maximum-scale = ' + phoneScale + ', target-densitydpi=device-dpi">');
+            // andriod 2.3以上
         } else {
-            document.write('<meta name="viewport" content="width=640,height=1030, user-scalable=no, target-densitydpi=device-dpi">');
+            document.write('<meta name="viewport" content="width=640, target-densitydpi=device-dpi">');
         }
-        document.addEventListener('touchmove', function(e){e.preventDefault()}, false);
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
+        // 其他系统
+    } else {
+        document.write('<meta name="viewport" content="width=640,height=1030, user-scalable=no, target-densitydpi=device-dpi">');
+    }
+    document.addEventListener('touchmove', function(e){e.preventDefault()}, false);
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
     </script>
     <style media="screen">
     *{ margin:0; padding:0; list-style:none;}
@@ -92,44 +92,44 @@
         </div>
     </div>
     @yield('content')
+    <div style="display:none;">
+        {!! $project->stats_code !!}
+    </div>
     <script type="text/javascript" charset="utf-8">
-        wx.config({!! $jssdk->jssdk->buildConfig([
-            'onMenuShareTimeline',
-            'onMenuShareAppMessage',
-            'onMenuShareQQ ',
-            'onMenuShareWeibo',
-            'chooseImage',
-            'uploadImage'
-        ], false) !!});
-        wx.ready(function(){
-            wx.onMenuShareAppMessage({
-                title: "<?=$project->share_title?>", // 分享标题
-                desc: "<?=$project->share_desc?>", // 分享描述
-                link: "<?=route('app', [$project->id, 'index'])?>", // 分享链接
-                imgUrl: "<?=$project->share_img?>", // 分享图标
-                success: function () {
-                    $.ajax({
-                		url: "{{ route('app', [$project->id, 'share']) }}",
-                	})
-                },
-                cancel: function () { }
-            });
-            wx.onMenuShareTimeline({
-                title: "<?=$project->timeline_share_title?>", // 分享标题
-                desc: "<?=$project->share_desc?>", // 分享描述
-                link: "<?=request()->url()?>", // 分享链接
-                imgUrl: "<?=$project->share_img?>", // 分享图标
-                success: function () {
-                    $.ajax({
-                		url: "{{ route('app', [$project->id, 'share']) }}",
-                	})
-                },
-                cancel: function () { }
-            });
+    wx.config({!! $jssdk->jssdk->buildConfig([
+        'onMenuShareTimeline',
+        'onMenuShareAppMessage',
+        'onMenuShareQQ ',
+        'onMenuShareWeibo',
+        'chooseImage',
+        'uploadImage'
+    ], false) !!});
+    wx.ready(function(){
+        wx.onMenuShareAppMessage({
+            title: "<?=$project->share_title?>", // 分享标题
+            desc: "<?=$project->share_desc?>", // 分享描述
+            link: "<?=route('app', [$project->id, 'index'])?>", // 分享链接
+            imgUrl: "<?=$project->share_img?>", // 分享图标
+            success: function () {
+                $.ajax({
+                    url: "{{ route('app', [$project->id, 'share']) }}",
+                })
+            },
+            cancel: function () { }
         });
-    </script>
-    {!! $project->stats_code !!}
-    <script>
+        wx.onMenuShareTimeline({
+            title: "<?=$project->timeline_share_title?>", // 分享标题
+            desc: "<?=$project->share_desc?>", // 分享描述
+            link: "<?=request()->url()?>", // 分享链接
+            imgUrl: "<?=$project->share_img?>", // 分享图标
+            success: function () {
+                $.ajax({
+                    url: "{{ route('app', [$project->id, 'share']) }}",
+                })
+            },
+            cancel: function () { }
+        });
+    });
     var la = {
         changeClass: function (target,id) {
             var className = $(target).attr('class');
