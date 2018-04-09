5108e5758bb6f37a2f190b6e67c9748810f72003
LiangPi <609671961@qq.com>
2018-04-09 13:50:06 +0000

    add wechat pay and ticket project type

--- app/Http/Controllers/Projects/TicketController.php
+++ app/Http/Controllers/Projects/TicketController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 
 class TicketController extends ProjectController{
     public function index(){
+        dd(session('wechat_user'));
         $order = new Order();
         $result = $order->createOrder(1, ['openid' => session('wechat_user')['openid']], true);
         dd($result);
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -13,7 +13,6 @@ class Order extends Model {
             'body' => '腾讯充值中心-QQ会员充值',
             'out_trade_no' => '20150806125346',
             'total_fee' => 88,
-            'notify_url' => '', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
             'trade_type' => 'JSAPI',
             'openid' => $order_info['openid'],
         ]);
