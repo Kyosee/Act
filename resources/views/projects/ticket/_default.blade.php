cfa28b5f974eec862adb6ff1a18ca2c8827da667
LiangPi <609671961@qq.com>
2018-04-09 14:49:45 +0000

    add wechat pay and ticket project type

--- resources/views/projects/ticket/_default.blade.php
+++ resources/views/projects/ticket/_default.blade.php
@@ -2,7 +2,7 @@
 <!doctype html>
 <html>
 <head>
-    <title>{{ $project->project_name }}</title>
+    <title></title>
     <meta charset="utf-8" />
     <meta name="apple-touch-fullscreen" content="YES" />
     <meta name="format-detection" content="telephone=no" />
@@ -88,43 +88,9 @@
 <body>
     @yield('content')
     <div style="display:none;">
-        {!! $project->stats_code !!}
     </div>
     <script type="text/javascript" charset="utf-8">
-        wx.config({!! $jssdk->jssdk->buildConfig([
-            'onMenuShareTimeline',
-            'onMenuShareAppMessage',
-            'onMenuShareQQ ',
-            'onMenuShareWeibo',
-            'chooseImage',
-            'uploadImage'
-        ], false) !!});
-        wx.ready(function(){
-            wx.onMenuShareAppMessage({
-                title: "<?=$project->share_title?>", // 分享标题
-                desc: "<?=$project->share_desc?>", // 分享描述
-                link: "<?=route('app', [$project->id, 'index'])?>", // 分享链接
-                imgUrl: "<?=$project->share_img?>", // 分享图标
-                success: function () {
-                    $.ajax({
-                        url: "{{ route('app', [$project->id, 'share']) }}",
-                    })
-                },
-                cancel: function () { }
-            });
-            wx.onMenuShareTimeline({
-                title: "<?=$project->timeline_share_title?>", // 分享标题
-                desc: "<?=$project->share_desc?>", // 分享描述
-                link: "<?=request()->url()?>", // 分享链接
-                imgUrl: "<?=$project->share_img?>", // 分享图标
-                success: function () {
-                    $.ajax({
-                        url: "{{ route('app', [$project->id, 'share']) }}",
-                    })
-                },
-                cancel: function () { }
-            });
-        });
+
     </script>
 </body>
 </html>
