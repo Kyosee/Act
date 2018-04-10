1cb1b1ca58c79afdc89e56ef5edceb020e5100ac
LiangPi <609671961@qq.com>
2018-04-10 06:33:47 +0000

    wechat pay

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -27,15 +27,15 @@ class Order extends Model {
      * @return [type]              [description]
      */
     public function createOrder($wechat_id, $order_info, $is_sub = false){
-        dd($this->makeOutTradeNo());
         $app = Wechat::buildPayConfig($wechat_id, $is_sub);
         $result = $app->order->unify([
-            'body' => '腾讯充值中心-QQ会员充值',
-            'out_trade_no' => '20150806125346',
-            'total_fee' => 1,
+            'body' => $order_info['body'] ? $order_info['body'] : '用户订单',
+            'out_trade_no' => $order_info['out_trade_no'] ? $order_info['out_trade_no'] : $this->makeOutTradeNo(),
+            'total_fee' => $order_info['total_fee'] ? $order_info['total_fee'] : 1,
             'trade_type' => 'JSAPI',
             'openid' => $order_info['openid'],
         ]);
+        dd($result);
         return $app->jssdk->sdkConfig($result['prepay_id']);
     }
 }
