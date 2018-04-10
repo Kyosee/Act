9f61728fbc2dbd84c6d1b7995d1565af78cfac7b
LiangPi <609671961@qq.com>
2018-04-10 06:22:28 +0000

    wechat pay

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,7 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model {
 
+    public function saltMaker(){
+    	$str = "abcdefghijklmnopqrstuvwxyz";
+        $salt = "";
+        for($i = 0; $i < 5; $i++)
+            $salt .= $str[mt_rand(0,strlen($str)-1)];
+        return $salt;
+    }
+
+    public function makeOutTradeNo(){
+        return strtoupper(uniqid().$this->saltMaker().rand(1000, 9999));
+    }
+
+    /**
+     * create base order
+     * @param  [type]  $wechat_id  [description]
+     * @param  [type]  $order_info [description]
+     * @param  boolean $is_sub     [description]
+     * @return [type]              [description]
+     */
     public function createOrder($wechat_id, $order_info, $is_sub = false){
+        dd($this->makeOutTradeNo());
         $app = Wechat::buildPayConfig($wechat_id, $is_sub);
         $result = $app->order->unify([
             'body' => '腾讯充值中心-QQ会员充值',
